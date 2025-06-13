ab882c40ff8494be7be524bc23b839955119dd04
Emmakade <dmasterkay@gmail.com>
2025-06-13 03:27:50 +0100

    route web test

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/db-test', function () {
+    try {
+        \DB::connection()->getPdo();
+        return 'Database connected successfully.';
+    } catch (\Exception $e) {
+        return 'Failed to connect to the database: ' . $e->getMessage();
+    }
+});
+Route::get('/health', function () {
+    return response()->json(['status' => 'ok']);
+});
