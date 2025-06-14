fb92bbd2d48ce11bbf11d655afc843db335ba5b1
Emmakade <dmasterkay@gmail.com>
2025-06-14 22:27:09 +0100

    Reg screen showing homepage fix

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -11,6 +11,7 @@ class AuthController extends Controller
 {
     public function register(Request $request)
     {
+        $request->headers->set('Accept', 'application/json');
         $validated = $request->validate([
             'first_name' => 'required|string|max:255',
             'last_name' => 'required|string|max:255',
