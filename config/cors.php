bc4ac3ca6869946f2203718bcb306e67c1dfded8
Emmakade <dmasterkay@gmail.com>
2025-06-15 01:45:00 +0100

    cors allowed origin

--- config/cors.php
+++ config/cors.php
@@ -17,14 +17,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [
-        'http://localhost:3000',        // React
-        'http://127.0.0.1:3000',
-        'http://localhost:5000',        // Flutter
-        'http://127.0.0.1:5000',
-        'http://127.0.0.1:8000',
-        'https://finai-laravel.up.railway.app/', // Production domain
-    ],
+    'allowed_origins' => ['*'],  // Change to specific domains in production
 
     'allowed_origins_patterns' => [],
 
